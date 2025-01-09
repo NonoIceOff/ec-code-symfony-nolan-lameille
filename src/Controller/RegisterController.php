c1f409c16c3aa7b216e583b6478bc8c1522c17e9
NonoIceOff <nonocraft28@gmail.com>
2025-01-09 14:19:35 +0100

    first commit

---
+++ src/Controller/RegisterController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+use App\Entity\User;
+use App\Form\RegistrationFormType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+
+
+
+class RegisterController extends AbstractController
+{
+
+    private ValidatorInterface $validator;
+
+    public function __construct(ValidatorInterface $validator)
+    {
+        $this->validator = $validator;
+    }
+
+    #[Route('/register', name: 'auth_register')]
+    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, Security $security, EntityManagerInterface $entityManager): Response
+    {
+        $user = new User();
+        $errors = $this->validator->validate($user);
+        $form = $this->createForm(RegistrationFormType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            /** @var string $plainPassword */
+            $plainPassword = $form->get('plainPassword')->getData();
+
+            // encode the plain password
+            $user->setPassword($userPasswordHasher->hashPassword($user, $plainPassword));
+
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            // do anything else you need here, like send an email
+            return $this->redirectToRoute('auth_login');
+
+        }
+
+        // Validation de l'objet User
+        $errors = $this->validator->validate($user);
+
+        // Render the 'hello.html.twig' template
+        return $this->render('auth/register.html.twig', [
+            'name' => 'Thibaud', // Pass data to the view
+            'registrationForm' => $form,
+            'error' => $errors,
+        ]);
+    }
+}
